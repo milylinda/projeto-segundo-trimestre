6edd87c1abb67b7c9983c8a941c03c9861726f43
jamilylinda <143099405+milylinda@users.noreply.github.com>
2024-08-13 16:38:23 -0300

    djvf

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativa:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativa:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
