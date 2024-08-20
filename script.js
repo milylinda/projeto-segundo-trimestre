64ea62f2308eac41c633a2bc4cc3658492063abd
jamilylinda <143099405+milylinda@users.noreply.github.com>
2024-08-20 16:24:21 -0300

    jhgf

--- script.js
+++ script.js
@@ -5,18 +5,47 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".caixa-resultado");
 
 const perguntas = [
+    { 
+        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
+        alternativa:[
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
+        alternativa:[
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+        ]
+    }, 
+    {
+        enunciado: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+        alternativa:[
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, Gabriel precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativa:[
+            "Criar uma imagem utilizando um gerador de imagem de IA.",
+            "Criar uma imagem utilizando uma plataforma de design como o Paint."
+        ]
+    },
+    {
+        enunciado: "Achou assustador pensar que máquinas agora poderiam mudar o mundo. Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos. Notou que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
         alternativa:[
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
-    {
-        enunciado: "Pergunta 2",
-        alternativa:[
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
-    }
 ];
+
+    let atual = 0; 
+    let perguntaatual; 
+
+    function mostrapergunta(){
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+    }
